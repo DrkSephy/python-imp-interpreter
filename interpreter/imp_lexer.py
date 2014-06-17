b4a3cd50dfaf0737bb002cbe7a09888a8728ad45
David Leonard <sephirothcloud1025@yahoo.com>
2014-06-16 23:12:25 -0400

    Adding regex for matching whitespace and comments in lexer

--- interpreter/imp_lexer.py
+++ interpreter/imp_lexer.py
@@ -7,3 +7,20 @@
 #   1. RESERVED : tag indicator for a reserved word or operator
 #   2. INT : tag indicator for a literal integer
 #   3. ID : tag indicator for identifiers
+
+import lexer
+
+# Bind tags for keywords
+
+RESERVED = 'RESERVED'
+INT      = 'INT'
+ID       = 'ID'
+
+# Next, we define token expressions which will be used in the lexer.
+# We start with the characters we will drop (whitespace and comments)
+
+token_exprs = [
+    (r'[ \\n\\t]+',         None),   # match whitespace
+    (r'#[^\\n]*',           None),   # match comments
+]
+
