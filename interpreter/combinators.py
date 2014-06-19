c0a741ff48303548cb6f6bf9854cc45df7fd72d4
David Leonard <sephirothcloud1025@yahoo.com>
2014-06-18 22:24:22 -0400

    Adding reserved keyword parser

--- interpreter/combinators.py
+++ interpreter/combinators.py
@@ -69,5 +69,13 @@ class Parser:
 #       token[0] : value
 #       token[1] : tag
 
+class Reserved(Parser):
+    def __init__(self, value, tag):
+        self.value = value
+        self.tag = tag
 
-
+    def __call__(self, tokens, pos):
+        if pos < len(tokens) and tokens[pos][0] == self.value and tokens[pos][1] is self.tag:
+            return Result(tokens[pos][0], pos + 1)
+        else:
+            return None 
