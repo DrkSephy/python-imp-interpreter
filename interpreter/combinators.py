a9c2eae5b824139248b3bcd380dc94a2a8225a38
David Leonard <sephirothcloud1025@yahoo.com>
2014-06-21 16:17:49 -0400

    Implemented Alternate parser

--- interpreter/combinators.py
+++ interpreter/combinators.py
@@ -128,3 +128,15 @@ class Concat(Parser):
 # the left parser, and if successful that result is returned. If unsuccessful, 
 # it applies the right parser and returns its result.
 
+class Alternate(Parser):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+    def __call__(self, tokens, pos):
+        left_result = self.left(tokens, pos)
+        if left_result:
+            return left_result
+        else:
+            right_result = self.right(tokens, pos)
+            return right_result
