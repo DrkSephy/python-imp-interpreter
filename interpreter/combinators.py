68735fe0024c85e6a2e2e6530b92281e5aaf2bc7
David Leonard <sephirothcloud1025@yahoo.com>
2014-06-26 13:58:21 -0400

    Implemented Phrase parser

--- interpreter/combinators.py
+++ interpreter/combinators.py
@@ -238,3 +238,14 @@ class Lazy(Parser):
 # that it will fail if its input parser does not consume all of the remaining tokens. 
 # The top level parser for IMP will be a `Phrase` parser. This prevents us from 
 # partially matching a program which has garbage at the end. 
+
+class Phrase(Parser):
+    def __init__(self, parser):
+        self.parser = parser
+
+    def __call__(self, tokens, pos):
+        result = self.parser(tokens, pos)
+        if result and result.pos == len(tokens):
+            return result
+        else:
+            return None
