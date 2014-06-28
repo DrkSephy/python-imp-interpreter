992e556bd0adb2b46227775d0d9a447a4be028e8
David Leonard <sephirothcloud1025@yahoo.com>
2014-06-28 12:55:12 -0400

    Adding implementation of expression parser

--- interpreter/combinators.py
+++ interpreter/combinators.py
@@ -265,3 +265,23 @@ class Phrase(Parser):
 # parser must return a function which combines elements parsed on the left and right 
 # into a single value. The value is accumulated for the whole list, left -> right, 
 # and is returned. 
+
+class Exp(Parser):
+    def __init__(self, parser, separator):
+        self.parser = parser
+        self.separator = separator
+
+    def __call__(self, tokens, pos):
+        result = self.parser(tokens, pos)
+
+        def process_next(parsed):
+            (sepfunc, right) = parsed
+            return sepfunc(result.value, right)
+        next_parser = self.separator + self.parser ^ process_next
+
+        next_result = result
+        while next_result:
+            next_result = next_parser(tokens, result.pos)
+            if next_result:
+                result = next_result
+        return result  
