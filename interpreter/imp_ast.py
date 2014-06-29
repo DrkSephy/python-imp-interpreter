8fc000586cb8e969350ea71322906f16ec80a7b3
David Leonard <sephirothcloud1025@yahoo.com>
2014-06-29 08:46:47 -0400

    Adding base file for imp ast

---
+++ interpreter/imp_ast.py
@@ -0,0 +1,6 @@
+# imp_ast.py
+# ----------
+# This file defines the data structures we will be the output of our 
+# parser. We will write our parser using our combinator library which 
+# will convert the list of tokens returned by the lexer into an AST. 
+# Once the AST is finished, we can easily execute the program. 
