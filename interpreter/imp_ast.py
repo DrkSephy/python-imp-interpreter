822c2fca953f841fdb8a431620d56096f8594427
David Leonard <sephirothcloud1025@yahoo.com>
2014-07-02 10:56:46 -0400

    Adding variable expression class for building AST

--- interpreter/imp_ast.py
+++ interpreter/imp_ast.py
@@ -39,3 +39,11 @@ class IntAexp(Aexp):
 
     def __repr__(self):
         return 'IntAexp(%d)' % self.i
+
+class VarAexp(Aexp):
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return 'VarAexp(%s)' % self.name
+
