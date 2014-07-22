f26ca1d02d5dc3154d6df7161f1550cc8b18afb2
David Leonard <sephirothcloud1025@yahoo.com>
2014-07-21 22:52:02 -0400

    Updating imp lexer

--- interpreter/imp_lexer.py
+++ interpreter/imp_lexer.py
@@ -24,40 +24,33 @@ ID       = 'ID'
 # engine to escape operators like "+" and "*".
 
 token_exprs = [
-    (r'[ \\n\\t]+',         None),   # match whitespace
-    (r'#[^\\n]*',           None),   # match comments
-
-# Now, we need expressions for matching operators and reserved words. 
-
-    (r'\:=',                RESERVED), 
-    (r'\(',                 RESERVED),
-    (r'\)',                 RESERVED),
-    (r';',                  RESERVED),
-    (r'\+',                 RESERVED),
-    (r'-',                  RESERVED), 
-    (r'\*',                 RESERVED),
-    (r'/',                  RESERVED),
-    (r'<=',                 RESERVED),
-    (r'<',                  RESERVED),
-    (r'>=',                 RESERVED),
-    (r'>',                  RESERVED),
-    (r'=',                  RESERVED),
-    (r'!=',                 RESERVED),
-    (r'and',                RESERVED),
-    (r'or',                 RESERVED),
-    (r'not',                RESERVED),
-    (r'if',                 RESERVED),
-    (r'then',               RESERVED),
-    (r'else',               RESERVED),
-    (r'while',              RESERVED),
-    (r'do',                 RESERVED),
-    (r'end',                RESERVED),
-
-    # Regular expressions for integers and identifiers. 
-    # The regex for identifiers would match all of the 
-    # reserved words above, so we put this one last.
-    (r'[0-9]+',                 INT),
-    (r'[A-Za-z][A-Za-z0-9_]*',  ID),
+    (r'[ \n\t]+',              None),
+    (r'#[^\n]*',               None),
+    (r'\:=',                   RESERVED),
+    (r'\(',                    RESERVED),
+    (r'\)',                    RESERVED),
+    (r';',                     RESERVED),
+    (r'\+',                    RESERVED),
+    (r'-',                     RESERVED),
+    (r'\*',                    RESERVED),
+    (r'/',                     RESERVED),
+    (r'<=',                    RESERVED),
+    (r'<',                     RESERVED),
+    (r'>=',                    RESERVED),
+    (r'>',                     RESERVED),
+    (r'!=',                    RESERVED),
+    (r'=',                     RESERVED),
+    (r'and',                   RESERVED),
+    (r'or',                    RESERVED),
+    (r'not',                   RESERVED),
+    (r'if',                    RESERVED),
+    (r'then',                  RESERVED),
+    (r'else',                  RESERVED),
+    (r'while',                 RESERVED),
+    (r'do',                    RESERVED),
+    (r'end',                   RESERVED),
+    (r'[0-9]+',                INT),
+    (r'[A-Za-z][A-Za-z0-9_]*', ID),
 ]
 
 # Create our lexer function
