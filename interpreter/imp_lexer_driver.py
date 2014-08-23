36a0e05469b5d1077fad178c0155e3e863d57926
David Leonard <sephirothcloud1025@yahoo.com>
2014-08-23 17:07:20 -0400

    Updating driver for imp lexer

--- interpreter/imp_lexer_driver.py
+++ interpreter/imp_lexer_driver.py
@@ -1,12 +1,10 @@
-# Driver code for testing out our lexer
-
 import sys
 from imp_lexer import *
 
 if __name__ == '__main__':
     filename = sys.argv[1]
     file = open(filename)
-    characters = file.reaD()
+    characters = file.read()
     file.close()
     tokens = imp_lex(characters)
     for token in tokens:
