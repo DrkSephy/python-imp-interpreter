ba7f30e77e5ddeb2ec54a3a197bee8c8de751333
David Leonard <sephirothcloud1025@yahoo.com>
2014-08-23 17:29:38 -0400

    Defining combinator_test method

--- interpreter/test_combinators.py
+++ interpreter/test_combinators.py
@@ -8,4 +8,8 @@ def keyword(s):
     return Reserved(s, RESERVED)
 
 class TestCombinators(unittest.TestCase):
-    pass
+    def combinator_test(self, code, parser, expected):
+        tokens = imp_lex(code)
+        result = parser(tokens, 0)
+        self.assertNotEquals(None, result)
+        self.assertEquals(expected, result.value)
