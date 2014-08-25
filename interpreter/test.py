a34236489882d5aac5556e52239c0eb3bde33504
David Leonard <sephirothcloud1025@yahoo.com>
2014-08-25 12:38:39 -0400

    Adding test_imp_parser to test runner test.py

--- interpreter/test.py
+++ interpreter/test.py
@@ -2,6 +2,6 @@ import sys
 import unittest
 
 if __name__ == '__main__':
-    test_names = ['test_lexer', 'test_combinators', 'test_eval']
+    test_names = ['test_lexer', 'test_combinators', 'test_eval', 'test_imp_parser']
     suite = unittest.defaultTestLoader.loadTestsFromNames(test_names)
     result = unittest.TextTestRunner().run(suite)
